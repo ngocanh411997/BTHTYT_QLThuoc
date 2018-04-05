02b212e263a4d400c6a8615910adeb07da700255
NgocAnh <ngocanh411997@gmail.com>
2018-04-05 21:46:46 +0700

    [Ánh] Connect Controller

---
+++ QLThuoc/QLThuoc/controller/setupConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLThuoc.controller
+{
+    public class setupConnection
+    {
+        public static Func<DbConnection> ConnectionFactory = () => new SqlConnection(ConnectionString.Connection);
+
+        public static class ConnectionString
+        {
+            public static string Connection = "Data Source=NGOCANH\\NGOCANH;Initial Catalog=QLGV_HS_THPT;Integrated Security=True";
+        }
+    }
+}
