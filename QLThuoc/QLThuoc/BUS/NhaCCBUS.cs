49a58afceff847dabe18b1704ef03958ca1d2c6a
Hanhpi <hanhphanmta2010@gmail.com>
2018-04-18 15:37:02 +0700

    NhaCC

---
+++ QLThuoc/QLThuoc/BUS/NhaCCBUS.cs
@@ -0,0 +1,42 @@
+﻿using QLThuoc.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using QLThuoc.models;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLThuoc.BUS
+{
+   public class NhaCCBUS
+    {
+        NhaCCDAL da = new NhaCCDAL();
+
+        public DataTable GetData()
+        {
+            return da.GetData();
+        }
+
+        public string TangMa()
+        {
+            return da.TangMa();
+        }
+        public int InsertData(NhaCC NCC)
+        {
+            return da.InsertData(NCC);
+        }
+        public int UpdateData(NhaCC NCC)
+        {
+            return da.UpdateData(NCC);
+        }
+        public int DeleteData(String ID)
+        {
+            return da.DeleteData(ID);
+        }
+        public DataTable TimKiemNCC(string strTimKiem)
+        {
+            return da.TimKiemNCC(strTimKiem);
+        }
+    }
+}
